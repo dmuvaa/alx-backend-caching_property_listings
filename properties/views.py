586eee528671e6b790db1a673ad78de23e9ad66f
Dennis Muvaa <dmuvaa70@gmail.com>
2025-11-02 23:47:37 +0300

    Cache Property List View

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
+
+@cache_page(60 * 15)  # 15 minutes
+def property_list(request):
+    """Return a lightweight JSON payload (no serialization lib needed here)"""
+    data = list(
+        Property.objects.values(
+            "id", "title", "description", "price", "location", "created_at"
+        )
+    )
+    return JsonResponse(data, safe=False)
